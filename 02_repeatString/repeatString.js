0aed63146c09cb3368bba328fb7e76d91e371e98
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2023-12-06 08:38:29 -0600

    02_repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, n) {
+    let finalString="";
+    if(n<0){
+        return "ERROR"
+    }else {
+        for (let i = 0; i < n; i++) {
+            finalString+=text;        
+        }
+        return finalString;
+    }
 };
 
 // Do not edit below this line
