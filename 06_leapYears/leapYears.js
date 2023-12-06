9853374f419328e2ea31b37a2fb09f6751e443e6
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2023-12-06 09:33:19 -0600

    06_leapYears completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeap = false;
+    if(year%4==0){        
+        if(year%100==0&&year%400!=0){
+            return false
+        }else{
+            return isLeap=true
+        }
+    }else{
+        return isLeap=false;
+    }
 };
 
 // Do not edit below this line
