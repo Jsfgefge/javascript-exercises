266ec9bbbd14d637bda06e8240d3f56d10da522a
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2023-12-06 08:47:22 -0600

    03_reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let newText ="";
+    for (let i = 0; i < text.length; i++) {
+        newText+=text[text.length-(i+1)];        
+    }
+    return newText;
 };
 
 // Do not edit below this line
