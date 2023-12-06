8d74f00a75b1783a71e02edd2069f0d0c38fc8d7
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2023-12-06 09:16:07 -0600

    04_removeFromArray completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(Arr,...args) {
+    let mainArr = Arr;
+    let parameters = args;
+    parameters.forEach(element => {
+        const index = mainArr.indexOf(element);
+        if(index<0){
+        }else{
+            mainArr.splice(index,1);
+        }
+    });
+    
+    return mainArr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
