1e7db28fe2a06d0f5f57badc7218419f7960a833
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2023-12-06 09:25:03 -0600

    05_sumAll completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+    if (typeof n1 != "number" || typeof n2 != "number" || n1<0||n2<0) {
+        return "ERROR";
+    } else {
+        let max = Math.max(n1, n2);
+        let min = Math.min(n1, n2);
+        let total = 0;
 
+        for (let i = min - 1; i < max + 1; i++) {
+            total += i;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
